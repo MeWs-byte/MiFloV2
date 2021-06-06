8ed90d9ef98c3d7d77c08b72fe5a7d89890cd811
heyoo <mackillopcalum@gmail.com>
2021-06-06 18:57:59 +0200

    went back to previous, might have lost intro , changed audio paths again

--- playsounds.py
+++ playsounds.py
@@ -3,9 +3,9 @@ import time
 
 def alarmSound():
     
-    call(["aplay", "/home/pi/FlipDotWorker/newFlo/newestFlo/sounds/pling.wav"]) 
+    call(["aplay", "sounds/pling.wav"]) 
 
-    call(["aplay", "/home/pi/FlipDotWorker/newFlo/newestFlo/sounds/islandComp.wav"])
+    call(["aplay", "sounds/islandComp.wav"])
 
 # "/home/pi/project/sou/sou1.wav"
 # /home/pi/project/sou/startrek.wav  
@@ -14,7 +14,7 @@ def alarmSound():
     
 def alarmSound2():
     
-    call(["aplay", "/home/pi/FlipDotWorker/newFlo/newestFlo/sounds/var.wav"]) 
+    call(["aplay", "sounds/var.wav"]) 
     print('alaaarm sounds')
     #call(["aplay", "/home/pi/FlipDotWorker/newFlo/newestFlo/sounds/flint.wav"])
 
